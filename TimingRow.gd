bf9f2dca7659916e0c88acc4ad4b9e2e831d971f
gManbio <max.heiligman@gmail.com>
2024-02-13 22:35:18 -0600

    Working with Instantiation!
    This version should be capable of doing any number of pilots.  I streamlined other aspects of the code.

---
+++ TimingRow.gd
@@ -0,0 +1,51 @@
+extends Control
+
+@onready var pilot_name = $"Timing Box/Name"
+
+@onready var lap = $"Timing Box/Lap"
+
+@onready var gate = $"Timing Box/Gate"
+
+@onready var delta = $"Timing Box/Delta"
+
+@onready var progress_bar = $"ProgressBar"
+
+func _ready():
+	pass
+
+
+func set_pilot_name(p_name, color):
+	pilot_name.text = str(p_name)
+	pilot_name.modulate = color
+
+
+func set_lap(lap_num):
+	lap.text = str(lap_num)
+
+
+func set_gate(gate_num):
+	gate.text = str(gate_num)
+
+
+func set_delta(delta_time):
+	delta.text = str(delta_time)
+
+
+func set_progress(prog_value, color):
+	progress_bar.value = prog_value
+	progress_bar.modulate = color
+
+
+func set_progress_range(min, max):
+	progress_bar.min_value = min
+	progress_bar.max_value = max
+
+
+func reset():
+	pilot_name.text = "--"
+	pilot_name.modulate = Color(Color.WHITE)
+	lap.text = "--"
+	gate.text = "--"
+	delta.text = "--"
+	progress_bar.value = 0
+	progress_bar.modulate = Color(Color.WHITE)
--- ws_new_main.gd
+++ ws_new_main.gd
@@ -3,132 +3,39 @@ extends Node
 var ws = WebSocketPeer.new()
 
 var heatData = []
+
 var pilots = []
 
-var name_p1 = ""
-var name_p2 = ""
-var name_p3 = ""
-var name_p4 = ""
-var name_p5 = ""
-var name_p6 = ""
+@onready var bg_rect = $ColorRect
+@onready var connect_button = $"Control/Connect Button"
+@onready var first_place_celebration = $Control/Orb
+@onready var score_a = $"Control/ScoreContainer/Score A"
+@onready var score_b = $"Control/ScoreContainer/Score B"
 
-var gate_p1 = ""
-var gate_p2 = ""
-var gate_p3 = ""
-var gate_p4 = ""
-var gate_p5 = ""
-var gate_p6 = ""
-
-var delta_p1 = ""
-var delta_p2 = ""
-var delta_p3 = ""
-var delta_p4 = ""
-var delta_p5 = ""
-var delta_p6 = ""
-
-var slap_p1 = ""
-var slap_p2 = ""
-var slap_p3 = ""
-var slap_p4 = ""
-var slap_p5 = ""
-var slap_p6 = ""
-
-var prog_p1 = ""
-var prog_p2 = ""
-var prog_p3 = ""
-var prog_p4 = ""
-var prog_p5 = ""
-var prog_p6 = ""
-
-var p1_list = ""
-var p2_list = ""
-var p3_list = ""
-var p4_list = ""
-var p5_list = ""
-var p6_list = ""
-
-var display_list = ""
-var gate_count = 30
 var ip_complete = false
-var bg_rect = ""
-var connect_button = ""
 var connected = false
 var single_lap_display = false
-var first_place_celebration = ""
 var unbursted = true
 var last_message = {}
 var team_mode = false
-var score_a = ""
-var score_b = ""
 var score_board = {}
 var new_score = true
 var team_order = []
+
+var timing_row = preload("res://TimingRow.tscn")
+
+#fall back default values
+var gate_count = 30
 var race_laps = 3
 
-var timing_row = load("res://TimingRow.tscn")
-
 func _ready():
-	
 	Engine.max_fps = 30
 	
 	var ip_addresses = IP.get_local_addresses()
 	var ip_input = $Control/IP_Input
 	ip_input.text = ip_addresses[-1]
-
+	
 	$HeartbeatTimer.start()
-	
-	name_p1 = $"Control/VBoxContainer/Hbox-p1/Name"
-	name_p2 = $"Control/VBoxContainer/Hbox-p2/Name"
-	name_p3 = $"Control/VBoxContainer/Hbox-p3/Name"
-	name_p4 = $"Control/VBoxContainer/Hbox-p4/Name"
-	name_p5 = $"Control/VBoxContainer/Hbox-p5/Name"
-	name_p6 = $"Control/VBoxContainer/Hbox-p6/Name"
-	
-	gate_p1 = $"Control/VBoxContainer/Hbox-p1/Gate"
-	gate_p2 = $"Control/VBoxContainer/Hbox-p2/Gate"
-	gate_p3 = $"Control/VBoxContainer/Hbox-p3/Gate"
-	gate_p4 = $"Control/VBoxContainer/Hbox-p4/Gate"
-	gate_p5 = $"Control/VBoxContainer/Hbox-p5/Gate"
-	gate_p6 = $"Control/VBoxContainer/Hbox-p6/Gate"
-	
-	delta_p1 = $"Control/VBoxContainer/Hbox-p1/Delta"
-	delta_p2 = $"Control/VBoxContainer/Hbox-p2/Delta"
-	delta_p3 = $"Control/VBoxContainer/Hbox-p3/Delta"
-	delta_p4 = $"Control/VBoxContainer/Hbox-p4/Delta"
-	delta_p5 = $"Control/VBoxContainer/Hbox-p5/Delta"
-	delta_p6 = $"Control/VBoxContainer/Hbox-p6/Delta"
-
-	slap_p1 = $"Control/VBoxContainer/Hbox-p1/Sector _ Lap"
-	slap_p2 = $"Control/VBoxContainer/Hbox-p2/Sector _ Lap"
-	slap_p3 = $"Control/VBoxContainer/Hbox-p3/Sector _ Lap"
-	slap_p4 = $"Control/VBoxContainer/Hbox-p4/Sector _ Lap"
-	slap_p5 = $"Control/VBoxContainer/Hbox-p5/Sector _ Lap"
-	slap_p6 = $"Control/VBoxContainer/Hbox-p6/Sector _ Lap"
-
-	prog_p1 = $"Control/VBoxContainer/ProgressBar-p1"
-	prog_p2 = $"Control/VBoxContainer/ProgressBar-p2"
-	prog_p3 = $"Control/VBoxContainer/ProgressBar-p3"
-	prog_p4 = $"Control/VBoxContainer/ProgressBar-p4"
-	prog_p5 = $"Control/VBoxContainer/ProgressBar-p5"
-	prog_p6 = $"Control/VBoxContainer/ProgressBar-p6"
-	
-	p1_list = [name_p1, slap_p1, gate_p1, delta_p1, prog_p1]
-	p2_list = [name_p2, slap_p2, gate_p2, delta_p2, prog_p2]
-	p3_list = [name_p3, slap_p3, gate_p3, delta_p3, prog_p3]
-	p4_list = [name_p4, slap_p4, gate_p4, delta_p4, prog_p4]
-	p5_list = [name_p5, slap_p5, gate_p5, delta_p5, prog_p5]
-	p6_list = [name_p6, slap_p6, gate_p6, delta_p6, prog_p6]
-	
-	display_list = [p1_list, p2_list, p3_list, p4_list, p5_list, p6_list]
-	
-	first_place_celebration = $Control/Orb
-	
-	bg_rect = $ColorRect
-	
-	connect_button = $"Control/Connect Button"
-	
-	score_a = $"Control/ScoreContainer/Score A"
-	score_b = $"Control/ScoreContainer/Score B"
 
 
 func _process(delta):
@@ -177,7 +84,7 @@ func _process_message(pilotdata):
 			reset_leaderboard()
 	elif "racetype" in pilotdata:
 		if pilotdata["racetype"]["raceLaps"] != str(race_laps):
-			race_laps = str(pilotdata["racetype"]["raceLaps"])
+			race_laps = int(pilotdata["racetype"]["raceLaps"])
 	elif "racedata" in pilotdata:
 		for pilot_name in pilotdata["racedata"]:
 			_on_new_pilot_data_received(pilotdata["racedata"][pilot_name], pilot_name)
@@ -189,7 +96,6 @@ func _on_timer_timeout():
 
 
 func update_pilot_data(new_data, pilotname):
-		# new_data is a dictionary like the one you provided
 	for pilot_name in new_data.keys():
 		var found = false
 		for pilot in pilots:
@@ -202,10 +108,7 @@ func update_pilot_data(new_data, pilotname):
 					pilot["gate_dict"][lap_gate_key] = float(new_data["time"])
 					pilot["gate_key"] = lap_gate_key
 				pilot["data"] = new_data
-
 				found = true
-				#print(pilot["gate_list"])
-				#var gate_details = [int(pilot["data"]["lap"]), int(pilot["data"]["gate"]), float(pilot["data"]["time"]]
 				break
 		if not found:
 			# Add new pilot
@@ -241,67 +144,50 @@ func add_timing_row():
 	var timing_row_instance = timing_row.instantiate()
 	var timing_container = $Control/TimingContainer
 	$Control/TimingContainer.add_child(timing_row_instance)
-	#print(len(timing_container.get_children()))
-	#var timing_row_list = $Control/TimingContainer.get_children()
-	#display_list = []
-	#for each in timing_row_list:
-		#var item_list = each.get_children()[0].get_children()
-		#item_list.append(each.get_children()[1])
-		#display_list.append(item_list)
-		#print(item_list)
-		#print(display_list)
-		
+	timing_row_instance.set_progress_range(0, gate_count * race_laps)
+
+
 func make_leaderboard():
 	var index = 0
-	var timing_row_list = []
-	print(pilots)
-	print(len(pilots))
-	print(len($Control/TimingContainer.get_children()))
-	if len(pilots) > len($Control/TimingContainer.get_children()):
-		if len($Control/TimingContainer.get_children()) == 0:
-			display_list = []
+	if len(pilots) > $Control/TimingContainer.get_child_count():
 		for i in pilots:
 			add_timing_row()
-		timing_row_list = $Control/TimingContainer.get_children()
-		print(timing_row_list)
-		for each in timing_row_list:
-			var item_list = each.get_children()[0].get_children()
-			item_list.append(each.get_children()[1])
-			display_list.append(item_list)
-
-	if len(timing_row_list) > 0:
+			if len(pilots) == $Control/TimingContainer.get_child_count():
+				break
+	if $Control/TimingContainer.get_child_count() > 0:
 		for pilot in pilots:
-			if index > len(timing_row_list):
+			if index > $Control/TimingContainer.get_child_count():
 				break
 			var hex_color = pilot["data"]["colour"]
 			var color = Color("#" + hex_color)
-			display_list[index][0].text = pilot["name"]
-			display_list[index][0].modulate = color
-			display_list[index][2].text = pilot["data"]["lap"]
-			display_list[index][4].text = pilot["data"]["gate"]
-			display_list[index][7].modulate = color
+			var current_pos = $Control/TimingContainer.get_children()[index]
+			current_pos.set_pilot_name(pilot["name"], color)
+			current_pos.set_lap(pilot["data"]["lap"])
+			current_pos.set_gate(pilot["data"]["gate"])
+			
 			if single_lap_display:
-				display_list[index][7].value = len(pilot["gate_dict"].keys()) % gate_count
+				current_pos.set_progress_range(0, gate_count)
+				current_pos.set_progress(len(pilot["gate_dict"].keys()) % gate_count, color)
 			else:
-				display_list[index][7].value = len(pilot["gate_dict"].keys())
+				current_pos.set_progress_range(0, gate_count * race_laps)
+				current_pos.set_progress(len(pilot["gate_dict"].keys()), color)
 			
 			if index != 0:
 				if pilot["gate_key"] in pilots[index - 1]["gate_dict"]:
 					var leader_time = pilots[index - 1]["gate_dict"][pilot["gate_key"]]
 					var pilot_time = pilot["gate_dict"][pilot["gate_key"]]
 					if pilot["data"]["finished"] == "True":
-						display_list[index][6].text = str(pilot["data"]["time"])
+						current_pos.set_delta(pilot["data"]["time"])
 					else:
-						display_list[index][6].text = str(leader_time - pilot_time)
-					
+						current_pos.set_delta(leader_time - pilot_time)
 			else:
 				if pilot["data"]["finished"] == "True":
-					display_list[index][6].text = str(pilot["data"]["time"])
+					current_pos.set_delta(pilot["data"]["time"])
 					if unbursted:
 						first_place_celebration.burst()
 						unbursted = false
 				else:
-					display_list[index][6].text = "0.000"
+					current_pos.set_delta(0.000)
 			index += 1
 
 
@@ -350,24 +236,12 @@ func make_scoreboard(scores):
 
 func reset_leaderboard():
 	unbursted = true
-	return
-	for display in display_list:
-		var count = 0
-		for each in display:
-			if count < 4:
-				each.text = "---"
-				each.modulate = Color(Color.WHITE)
-			else:
-				each.value = 0
-				each.modulate = Color(Color.WHITE)
-			count += 1
+	for child in $Control/TimingContainer.get_children():
+		$Control/TimingContainer.remove_child(child)
+		child.queue_free()
 	score_board = {}
 	new_score = true
 	team_order = []
-	score_a.text = "-"
-	score_b.text = "-"
-	score_a.modulate = Color(Color.WHITE)
-	score_b.modulate = Color(Color.WHITE)
 
 
 func _on_Button_pressed():
@@ -376,23 +250,20 @@ func _on_Button_pressed():
 	ws.connect_to_url(url)
 	print("Attempting to connect to WebSocket server at " + url)
 	ip_complete = true
-	return
 	gate_count = int($"Control/Gate Count".text)
-	for each in display_list:
-		each[4].min_value = 0
-		each[4].max_value = gate_count * race_laps
 
 
 func _on_Barmode_toggle_pressed(toggled_on):
 	if toggled_on:
-		for each in display_list:
-			each[4].max_value = gate_count
 		single_lap_display = true
+		if $Control/TimingContainer.get_child_count() > 0:
+			for child in $Control/TimingContainer.get_children():
+				child.set_progress_range(0, gate_count)
 	else:
-		for each in display_list:
-			each[4].max_value = gate_count * 3
 		single_lap_display = false
-		
+		if $Control/TimingContainer.get_child_count() > 0:
+			for child in $Control/TimingContainer.get_children():
+				child.set_progress_range(0, gate_count * race_laps)
 
 
 func _on_TeamvsTeam_toggle_pressed(toggled_on):
