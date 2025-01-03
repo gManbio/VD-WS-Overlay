fef35992d7a29e7f3343148e0890e46ce8b9f0e1
gManbio <max.heiligman@gmail.com>
2025-01-03 11:06:31 -0600

    Settings Tweaks v1-1-0

--- Settings.gd
+++ Settings.gd
@@ -1,12 +1,18 @@
 extends Control
 
+var always_on_top = false
+var is_green = false
 
-func _on_check_button_toggled(toggled_on: bool):
+
+func _on_aot_button_toggled(toggled_on: bool):
 	# In Godot 4.0, the main window ID is 0 by default.
-	var window_id = 0
+	always_on_top = toggled_on
 
-	print(toggled_on)
-
+func _on_green_button_toggled(toggled_on: bool):
+	# In Godot 4.0, the main window ID is 0 by default.
+	is_green = toggled_on
+	
 
 func _on_menu_button_pressed():
 	get_tree().change_scene_to_file("res://Menu.tscn")
+
