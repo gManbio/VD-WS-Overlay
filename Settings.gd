72b91095c1931d758789bca51a9937b4f07224a0
gManbio <max.heiligman@gmail.com>
2025-01-17 09:07:35 -0600

    updated version
    settings tweaks still non functioning

--- Settings.gd
+++ Settings.gd
@@ -1,16 +1,16 @@
 extends Control
 
 var always_on_top = false
-var is_green = false
+var is_borderless = false
 
 
 func _on_aot_button_toggled(toggled_on: bool):
 	# In Godot 4.0, the main window ID is 0 by default.
 	always_on_top = toggled_on
 
-func _on_green_button_toggled(toggled_on: bool):
+func _on_borderless_button_toggled(toggled_on: bool):
 	# In Godot 4.0, the main window ID is 0 by default.
-	is_green = toggled_on
+	is_borderless= toggled_on
 	
 
 func _on_menu_button_pressed():
